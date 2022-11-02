c9942cf057c78b703ddf5beb4ce7ef95d0e098e9
alvaro vargas riveros <alvarovargas@iMac-de-alvaro.local>
2022-11-02 03:46:26 -0300

    modificacion seccion turismo y algunas cosas responsive

--- js/script.js
+++ js/script.js
@@ -14,3 +14,34 @@ function seleccionar(){
     document.getElementById("nav").classList = "";
     menuVisible = false;
 }
+
+const sr = ScrollReveal({
+    origin: "top",
+    distance: "60px",
+    duration: 2500,
+    delay: 400,
+    
+});
+
+sr.reveal(`.swiper2`, {
+interval: 100,
+});
+
+sr.reveal(`.swiper, .discount__img`, { origin: "left" });
+sr.reveal(`.about__img, .discount__data`, { origin: "right" });
+
+
+var swiper = new Swiper('.mySwiper', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+    loop: true,
+});
